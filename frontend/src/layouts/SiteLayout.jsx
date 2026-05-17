8fcb72ee87cf6fae565b875b648ada20dec5489f
Gloria Imperio <gloria@imperio.com>
2026-05-16 19:34:24 -0500

    fix(layout): change site layout background to neutral-950 to fix white side strips

--- frontend/src/layouts/SiteLayout.jsx
+++ frontend/src/layouts/SiteLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 export default function SiteLayout({ children }) {
     return (
-        <div className="min-h-screen flex flex-col bg-white text-white">
+        <div className="min-h-screen flex flex-col bg-neutral-950 text-white">
             <TopBar />
             <Navbar />
             <main className="flex-1 px-4 sm:px-6 md:px-8 lg:px-12 max-w-[100vw] overflow-x-hidden">{children}</main>
